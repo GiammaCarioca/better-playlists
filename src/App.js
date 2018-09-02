f0a9e777ccc7f6e2efa8b2d4fa1a949889eba233
Thiago Giammattey <giammattey@gmail.com>
2018-09-02 13:33:13 -0300

    added basic style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+import './styles/global';
+
+/* Styled Components */
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 54px;
+  padding-top: 60px;
+`;
+
+const AggregateContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding-top: 60px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-top: 40px;
+
+  input {
+    width: 200px;
+  }
+`;
+
+const PlaylistContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  padding-top: 40px;
+
+  li {
+    list-style-type: none;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+/* Dummy Components */
 
 const Aggregate = () => (
   <div>
@@ -7,35 +54,40 @@ const Aggregate = () => (
 );
 
 const Filter = () => (
-  <div>
+  <FilterContainer>
     <img src="" alt="" />
     <input type="" />
-    Filter
-  </div>
+  </FilterContainer>
 );
 
 const Playlist = () => (
-  <div>
-    <img src="" alt="" />
-    <h3>Playlist Name</h3>
-    <ul>
-      <li>Song 1</li>
-      <li>Song 2</li>
-      <li>Song 3</li>
-    </ul>
-  </div>
+  <PlaylistContainer>
+    <div>
+      <img src="" alt="" />
+      <h3>Playlist Name</h3>
+      <ul>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+      </ul>
+    </div>
+  </PlaylistContainer>
 );
 
 const App = () => (
   <div>
-    <h1>Better Playlists</h1>
-    <Aggregate />
-    <Aggregate />
+    <Title>Better Playlists</Title>
+    <AggregateContainer>
+      <Aggregate />
+      <Aggregate />
+    </AggregateContainer>
     <Filter />
-    <Playlist />
-    <Playlist />
-    <Playlist />
-    <Playlist />
+    <PlaylistContainer>
+      <Playlist />
+      <Playlist />
+      <Playlist />
+      <Playlist />
+    </PlaylistContainer>
   </div>
 );
 
