31b492a7897b9c40c1f83166856c88899b6dbe66
Thiago Giammattey <giammattey@gmail.com>
2018-09-02 11:39:09 -0300

    added dummy components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 import React from 'react';
 
-const App = () => <h1>Better Playlists</h1>;
+const Aggregate = () => (
+  <div>
+    <h2>Number Text</h2>
+  </div>
+);
+
+const Filter = () => (
+  <div>
+    <img src="" alt="" />
+    <input type="" />
+    Filter
+  </div>
+);
+
+const Playlist = () => (
+  <div>
+    <img src="" alt="" />
+    <h3>Playlist Name</h3>
+    <ul>
+      <li>Song 1</li>
+      <li>Song 2</li>
+      <li>Song 3</li>
+    </ul>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <h1>Better Playlists</h1>
+    <Aggregate />
+    <Aggregate />
+    <Filter />
+    <Playlist />
+    <Playlist />
+    <Playlist />
+    <Playlist />
+  </div>
+);
 
 export default App;
