f5676801329bbf4e417b63bdd4a85cebd4a405b6
Thiago Giammattey <giammattey@gmail.com>
2018-09-05 01:00:38 -0300

    added fake server data to title and aggregate counters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import './styles/global';
 
@@ -10,7 +10,7 @@ const Title = styled.h1`
   padding-top: 60px;
 `;
 
-const AggregateContainer = styled.div`
+const AggregatesContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
@@ -45,13 +45,84 @@ const PlaylistContainer = styled.div`
   }
 `;
 
+const Loading = styled.span`
+  font-size: 36px;
+`;
+
 /* Dummy Components */
 
-const Aggregate = () => (
-  <div>
-    <h2>Number Text</h2>
-  </div>
-);
+const fakeServerData = {
+  user: {
+    name: 'Giamma',
+    playlists: [
+      {
+        name: 'My favorites',
+        songs: [
+          { name: 'Love On The Brain', duration: 224 },
+          { name: 'Fire bomb', duration: 258 },
+          { name: 'Kiss It Better', duration: 253 },
+        ],
+      },
+      {
+        name: 'Discover Weekly',
+        songs: [
+          { name: 'U Got It Bad', duration: 268 },
+          { name: 'hard rain', duration: 301 },
+          { name: 'Fast Slow Disco', duration: 287 },
+        ],
+      },
+      {
+        name: 'Dance To This',
+        songs: [
+          { name: 'In My Feelings', duration: 308 },
+          { name: 'Missing U', duration: 291 },
+          { name: 'TOOTIMETOOTIMETOOTIME', duration: 299 },
+        ],
+      },
+      {
+        name: 'Rock My Soul',
+        songs: [
+          { name: 'Love It If We Made It', duration: 253 },
+          { name: 'One Thing Left To Try', duration: 260 },
+          { name: 'Little Dark Age', duration: 300 },
+        ],
+      },
+    ],
+  },
+};
+
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          {this.props.playlists.length}
+          {' '}
+playlists
+        </h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    const allSongs = this.props.playlists.reduce(
+      (songs, eachPlaylist) => songs.concat(eachPlaylist.songs),
+      [],
+    );
+    const totalDuration = allSongs.reduce((sum, eachSong) => sum + eachSong.duration, 0);
+    return (
+      <div>
+        <h2>
+          {Math.floor(totalDuration / 60)}
+          {' '}
+minutes
+        </h2>
+      </div>
+    );
+  }
+}
 
 const Filter = () => (
   <FilterContainer>
@@ -74,21 +145,45 @@ const Playlist = () => (
   </PlaylistContainer>
 );
 
-const App = () => (
-  <div>
-    <Title>Better Playlists</Title>
-    <AggregateContainer>
-      <Aggregate />
-      <Aggregate />
-    </AggregateContainer>
-    <Filter />
-    <PlaylistContainer>
-      <Playlist />
-      <Playlist />
-      <Playlist />
-      <Playlist />
-    </PlaylistContainer>
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.serverData.user ? (
+          <div>
+            <Title>
+              {this.state.serverData.user.name}
+              's Playlists
+            </Title>
+            <AggregatesContainer>
+              <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+              <HoursCounter playlists={this.state.serverData.user.playlists} />
+            </AggregatesContainer>
+            <Filter />
+            <PlaylistContainer>
+              <Playlist />
+              <Playlist />
+              <Playlist />
+              <Playlist />
+            </PlaylistContainer>
+          </div>
+        ) : (
+          <Loading>Loading...</Loading>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
